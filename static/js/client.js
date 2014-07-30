d70128c3117fc73c38cad576ad55eede83de42dd
diab123 <diab123@users.noreply.github.com>
2014-07-30 12:32:32 -0600

    Update client.js

--- static/js/client.js
+++ static/js/client.js
@@ -699,16 +699,16 @@
                 $('.container .currentDirection').html(current.direction);
                 $('.container .current').toggleClass('high', current.y > 180).toggleClass('low', current.y < 70)
             }
-            data = d[0].map(function (obj) { return scalebg(obj.y) > 10; }).map(function (obj) {
+            data = d[0].map(function (obj) { return scalebg(obj.y > 10; }).map(function (obj) {
                 var color = '';
                 switch (true) {
-                    case scalebg(obj.y) > 140):
+                    case scalebg(obj.y > 140):
                         color = 'yellow';
                         break;
-                    case scalebg(obj.y) >=70 && scalebg(obj.y) <= 140):
+                    case scalebg(obj.y >=70 && scalebg(obj.y <= 140):
                         color = 'green';
                         break;
-                    case scalebg(obj.y) < 70):
+                    case scalebg(obj.y < 70):
                         color = 'red';
                         break;
                 }
